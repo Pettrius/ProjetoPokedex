646a7181f9c176aa17c421cc77525a42bfe88556
Pettrius Paiva <pettriuspaiva@outlook.com>
2025-08-25 20:10:48 -0300

    Entrega 1: Funções criadas

---
+++ Pokedex/main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+// Função de Cadastrar Cidade
+string cadastrarCidade(string nome)
+{
+    cout << "Funcionalidade em construcao..." << endl;
+}
+
+// Função de Cadastrar Estrada
+string cadastrarEstrada(string nome)
+{
+    cout << "Funcionalidade em construcao..." << endl;
+}
+
+// Função de Buscar Centro Pokemon Mais Proximo
+string buscarCentroPokemonMaisProximo(string nomeCidade)
+{
+    cout << "Funcionalidade em construcao..." << endl;
+}
+
+// Função de Cadastrar Pokemon
+string cadastrarPokemon(string nome, string tipo)
+{
+    cout << "Funcionalidade em construcao..." << endl;
+}
+
+// Função de Remover Pokemon
+string removerPokemon(string nome)
+{
+    cout << "Funcionalidade em construcao..." << endl;
+}
+
+// Função de Listar Pokemons por Nome
+string listarPokemonsNome(string nome)
+{
+    cout << "Funcionalidade em construcao..." << endl;
+}
+
+// Função de Listar Pokemons por Tipo
+string listarPokemonsTipo(string tipo)
+{
+    cout << "Funcionalidade em construcao..." << endl;
+}
+
+// Função de Contar Pokemons de Cada Tipo
+int contarPokemonsTipo(int qtdTipo)
+{
+    cout << "Funcionalidade em construcao..." << endl;
+}
+
+// Função de Encontrar Pokemons Proximos
+string encontrarPokemonsProximos(string localizacao)
+{
+    cout << "Funcionalidade em construcao..." << endl;
+}
+
+int main()
+{
+
+}
